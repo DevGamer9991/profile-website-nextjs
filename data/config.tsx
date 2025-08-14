5f2014713a4e0c59be383e5d2360d1d006e50b2b
Parker Thornton <pathornton114@gmail.com>
2025-08-14 16:23:05 +0000

    Modify Template

---
+++ data/config.tsx
@@ -0,0 +1,99 @@
+import { Button } from '@chakra-ui/react'
+import { Link } from '@saas-ui/react'
+import { NextSeoProps } from 'next-seo'
+import { FaGithub, FaInstagram, FaTwitter } from 'react-icons/fa'
+import { FiCheck } from 'react-icons/fi'
+import { Logo } from './logo'
+import { Em } from '#components/typography'
+
+const siteConfig = {
+  logo: Logo,
+  seo: {
+    title: <Em>Parker Thornton</Em>,
+    description: 'The React component library for startups',
+  } as NextSeoProps,
+  termsUrl: '#',
+  privacyUrl: '#',
+  header: {
+    links: [
+      {
+        id: 'features',
+        label: 'Features',
+      },
+      {
+        id: 'pricing',
+        label: 'Pricing',
+      },
+      {
+        id: 'faq',
+        label: 'FAQ',
+      },
+      {
+        label: 'Contact Me',
+        href: '/contact',
+      },
+      {
+        label: 'View Github',
+        href: 'https://github.com/DevGamer9991',
+        variant: 'primary',
+      },
+    ],
+  },
+  footer: {
+    copyright: (
+      <>
+        Template Built by{' '}
+        <Link href="https://twitter.com/Pagebakers">Eelco Wiersma</Link><br />
+        Modified By Parker Thornton
+      </>
+    ),
+    links: [
+      {
+        href: 'mailto:pathornton114@gmail.com',
+        label: 'Contact',
+      },
+      {
+        href: 'https://instagram.com/DevGamer9991',
+        label: <FaInstagram size="14" />
+      },
+      {
+        href: 'https://x.com/Dev_Gamer9991',
+        label: <FaTwitter size="14" />,
+      },
+      {
+        href: 'https://github.com/saas-js/saas-ui',
+        label: <FaGithub size="14" />,
+      },
+    ],
+  },
+  signup: {
+    title: 'Start building with Saas UI',
+    features: [
+      {
+        icon: FiCheck,
+        title: 'Accessible',
+        description: 'All components strictly follow WAI-ARIA standards.',
+      },
+      {
+        icon: FiCheck,
+        title: 'Themable',
+        description:
+          'Fully customize all components to your brand with theme support and style props.',
+      },
+      {
+        icon: FiCheck,
+        title: 'Composable',
+        description:
+          'Compose components to fit your needs and mix them together to create new ones.',
+      },
+      {
+        icon: FiCheck,
+        title: 'Productive',
+        description:
+          'Designed to reduce boilerplate and fully typed, build your product at speed.',
+      },
+    ],
+  },
+}
+
+export default siteConfig
---
+++ data/logo.tsx
@@ -0,0 +1,7 @@
+import { chakra, HTMLChakraProps, useColorModeValue } from '@chakra-ui/react'
+
+export const Logo: React.FC<HTMLChakraProps<'svg'>> = (props) => {
+  return (
+    <h1>Parker Thornton</h1>
+  )
+}
