e2408dfbdb2e3a7530b3685dc5a2ed8bb17a5119
Parker Thornton <pathornton114@gmail.com>
2025-08-30 13:52:30 +0000

    Update

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,5 +1,5 @@
 import { MarketingLayout } from '#components/layout'
 
 export default function Layout(props: { children: React.ReactNode }) {
-  return <MarketingLayout>{props.children}</MarketingLayout>
+  return <MarketingLayout announcementProps={{title: "Pardon My Mess", description: "This page is a work in progress!", href: "https://parkerthornton.net/"}}>{props.children}</MarketingLayout>
 }
